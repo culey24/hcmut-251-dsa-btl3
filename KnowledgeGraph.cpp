969ed45a617f322ef8e5861734056b1fb3386a72
culey24 <bao.quach24hcmut@hcmut.edu.vn>
2025-12-07 23:46:38 +0700

    added VertexNode implementations

--- KnowledgeGraph.cpp
+++ KnowledgeGraph.cpp
@@ -1,4 +1,5 @@
 #include "KnowledgeGraph.h"
+#include "main.h"
 
 // =============================================================================
 // Class Edge Implementation
@@ -14,12 +15,20 @@ Edge<T>::Edge(VertexNode<T>* from, VertexNode<T>* to, float weight) {
 template <class T>
 string Edge<T>::toString() {
     // TODO: Return the string representation of the edge
-    // return dih
+    // return
     return "";
 }
 
 // TODO: Implement other methods of Edge:
+template <class T>
+bool Edge<T>::equals(Edge<T>* edge) {
+    return (this->from == edge->from && this->to == edge->to);
+}
 
+template <class T>
+bool Edge<T>::edgeEQ(Edge<T> *&edge1, Edge<T> *&edge2) {
+    return edge1->equals(edge2);       
+}
 
 // =============================================================================
 // Class VertexNode Implementation
@@ -35,8 +44,52 @@ VertexNode<T>::VertexNode(T vertex, bool (*vertexEQ)(T&, T&), string (*vertex2st
 }
 
 template <class T>
+T& VertexNode<T>::getVertex() {
+    return this->vertex;
+}
+
+template <class T>
 void VertexNode<T>::connect(VertexNode<T>* to, float weight) {
     // TODO: Connect this vertex to the 'to' vertex
+    Edge<T>* edging = new Edge<T>(this, to, weight);
+    this->adList.push_back(edging);
+    to->adList.push_back(edging);
+    this->outDegree_++;
+    to->inDegree_++;
+}
+
+template <class T>
+bool VertexNode<T>::equals(VertexNode<T>* node) {
+    return vertexEQ(this->vertex, node->vertex);
+}
+
+template <class T>
+void VertexNode<T>::removeTo(VertexNode<T>* to) {
+    for (int i = 0; i < adList.size(); i++) {
+        Edge<T>* edging = adList[i];
+        if (edging->to->equals(to)) {
+            adList.erase(adList.begin() + i);
+            delete edging;
+            this->outDegree_--;
+            to->inDegree_--;
+        }
+    }
+    return;
+}
+
+template <class T>
+int VertexNode<T>::inDegree() {
+    return this->inDegree_;
+}
+
+template <class T>
+int VertexNode<T>::outDegree() {
+    return this->outDegree_;
+}
+
+template <class T>
+string VertexNode<T>::toString() {
+    return "";
 }
 
 // =============================================================================
@@ -52,11 +105,57 @@ DGraphModel<T>::DGraphModel(bool (*vertexEQ)(T&, T&), string (*vertex2str)(T&)) 
 template <class T>
 DGraphModel<T>::~DGraphModel() {
     // TODO: Clear all vertices and edges to avoid memory leaks
+    this->clear();
+}
+
+template <class T>
+VertexNode<T>* DGraphModel<T>::getVertexNode(T& vertex) {
+    for (VertexNode<T>* node : nodeList) {
+        if (node->vertex == vertex) return node;
+    }
+    return nullptr;
+}
+
+template <class T>
+string DGraphModel<T>::vertex2Str(VertexNode<T>& node) {
+    return vertex2str(node.vertex);
+}
+
+template <class T>
+string DGraphModel<T>::edge2Str(Edge<T>& edge) {
+    return edge.toString();
 }
 
 template <class T>
 void DGraphModel<T>::add(T vertex) {
     // TODO: Add a new vertex to the graph
+    VertexNode<T>* newNode = new VertexNode<T>(vertex, this->vertexEQ, this->vertex2str);
+    nodeList.push_back(newNode);
+}
+
+template <class T>
+bool DGraphModel<T>::contains(T vertex) {
+    return (getVertexNode(vertex) != nullptr);
+}
+
+template <class T>
+float DGraphModel<T>::weight(T from, T to) {
+    VertexNode<T>* fromNode = getVertexNode(from);
+    VertexNode<T>* toNode = getVertexNode(to);
+    if (fromNode == nullptr || toNode == nullptr) throw VertexNotFoundException();
+    Edge<T>* edging = fromNode->getEdge(toNode);
+    if (edging == nullptr) throw EdgeNotFoundException();
+    return edging->weight;
+}
+template <class T>
+vector<T> DGraphModel<T>::getOutwardEdges(T from) {
+    VertexNode<T>* fromNode = getVertexNode(from);
+    if (fromNode == nullptr) throw VertexNotFoundException();
+    vector<T> vertexList;
+    for (Edge<T>* edging : fromNode->adList) {
+        vertexList.push_back(edging->to->vertex);
+    }     
+    return vertexList;
 }
 
 template <class T>
